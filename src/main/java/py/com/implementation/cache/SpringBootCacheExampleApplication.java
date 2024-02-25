4f460ee418bf209b3929324879e5436fa01854b8
Igor Javier Gimenez Mendoza <ijgx@bfamiliar.com.py>
2024-02-25 20:07:27 -0300

    commit initial

---
+++ src/main/java/py/com/implementation/cache/SpringBootCacheExampleApplication.java
@@ -0,0 +1,13 @@
+package py.com.implementation.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCacheExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/com/implementation/cache/SpringBootCacheExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.implementation.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
